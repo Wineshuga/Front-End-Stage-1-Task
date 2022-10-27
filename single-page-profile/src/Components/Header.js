02b0cc5ecabb138d7d6db2cb4cfb10d670e86346
wineshuga <uziwinnie@gmail.com>
2022-10-27 23:02:59 +0100

    style: create mobile view

--- single-page-profile/src/Components/Header.js
+++ single-page-profile/src/Components/Header.js
@@ -1,13 +1,16 @@
 import pic from "../pic.jpg";
 import Icon from "../Icon.png";
+import share from "../share-button.png"
 
 const Header = () => {
     return (
         <header className="App-header">
             <img src={pic} id="profile__img" alt="logo" />
             <h5>WineShuga</h5>
+            <p className="slack-name">Slack: Wineshuga</p>
             <img className="share-icon" src={Icon} alt="icon" />
-        </header>        
+            <img className="share-icon-mobile" src={share} alt="icon" />
+        </header>
     )
 }
 
--- single-page-profile/src/Components/Main.js
+++ single-page-profile/src/Components/Main.js
@@ -11,7 +11,7 @@ const Main = () => {
             <Link id="book__python" href="https://books.zuri.team/python-for-beginners?ref_id=wineshuga" text="Python Books" />
             <Link id="pitch" href="https://backgroundcheck.zuri.team/" text="Background Check for Coders" />
             <Link id="book__design" href="https://books.zuri.team/design-rules" text="Design Books" />
-            <a href="https://github.com/wineshuga" target="_blank" rel="noopener noreferrer"
+            <a href="https://slack.com/wineshuga" target="_blank" rel="noopener noreferrer"
             ><img src={slack} className="icon" alt="slack link" /></a>
             <a href="https://github.com/wineshuga" target="_blank" rel="noopener noreferrer"
             ><img src={github} className="icon" alt="github link" /></a>
