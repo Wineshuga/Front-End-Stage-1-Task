18e6f6b82748b0ea7c2f316152096d81851dbb2b
wineshuga <uziwinnie@gmail.com>
2022-11-04 21:31:40 +0100

    feat: add routes to pages

--- single-page-profile/src/App.js
+++ single-page-profile/src/App.js
@@ -1,21 +1,17 @@
 import React from "react";
-import Main from "./Components/Main"
+import Home from "./Pages/HomePage"
 import './App.css';
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import {Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Contact from "./Pages/Contact";
 
 
 function App() {
   return (
     <div className="App">
-      <Header/>
       <Routes>
-      <Route path="/"><Main/></Route>
-      <Route path="/contact" ><Contact/></Route>
+        <Route path="/" element={<Home />}/>
+        <Route path="/contact" element={<Contact />}/>
       </Routes>
-      <Footer/>
     </div>
   );
 }
